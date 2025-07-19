54227b9e79acde63044579b8afddc87a1155bc69
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-19 23:53:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-// PATH: app/page.tsx
-import HeroLeadMagnet from './components/home/HeroLeadMagnet'
-import TrustBar from './components/home/TrustBar'
-import ValueProposition from './components/home/ValueProposition'
-import LeadMagnetOffer from './components/home/LeadMagnetOffer'
-import SocialProof from './components/home/SocialProof'
-import ProfessionalShowcase from './components/home/ProfessionalShowcase'
-import ROICalculator from './components/home/ROICalculator'
-// Removed: import UrgencyBanner from './components/home/UrgencyBanner'
-// Removed: import FAQSection from './components/home/FAQSection'
-import FinalCTA from './components/home/FinalCTA'
+import ApulinkHomepage from './components/home/ApulinkHomepage'
 
 export default function HomePage() {
-  return (
-    <>
-      <HeroLeadMagnet />
-      <TrustBar />
-      <ValueProposition />
-      <LeadMagnetOffer />
-      <ProfessionalShowcase />
-      <SocialProof />
-      <ROICalculator />
-      {/* Removed: <UrgencyBanner /> */}
-      {/* Removed: <FAQSection /> */}
-      <FinalCTA />
-    </>
-  )
+  return <ApulinkHomepage />
 }
